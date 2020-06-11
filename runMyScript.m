30a2700381628c56af27ad26d9ead9a4eb0eb9a9
djf2717 <djf2717@163.com>
2020-06-11 10:42:15 +0800

    Add files via upload

---
+++ runMyScript.m
@@ -0,0 +1,31 @@
+%mdl = 'sf_car';
+% load_system(mdl)
+% simMode = get_param(mdl, 'SimulationMode');
+% set_param(mdl, 'SimulationMode', 'normal')
+% cs = getActiveConfigSet(mdl);
+% mdl_cs = cs.copy;
+% set_param(mdl_cs,...
+%          'SaveState','on','StateSaveName','xoutNew',...
+%          'SaveOutput','on','OutputSaveName','youtNew')
+% simOut = sim(mdl, mdl_cs);
+% save('data.mat','simOut');
+
+
+% Path to the example test file
+exampleFile = 'SystemTesting.mldatx';
+
+% Load the example test file
+tc=sltest.testmanager.load(exampleFile);
+
+% Run the test case and return an object with results data
+ResultsObj = run(tc);
+
+% View the test file in the test manager
+% sltest.testmanager.view;
+
+% Generate a report from the results data
+filePath = 'test_report.pdf';
+sltest.testmanager.report(ResultsObj,filePath,...
+          'Author','Test Engineer',...
+          'IncludeSimulationSignalPlots',true,...
+          'IncludeComparisonSignalPlots',true);
